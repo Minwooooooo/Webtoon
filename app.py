53b30010e5bea3747e317a1afd2afc8895c55a49
jeongseoe <109732299+jeongseoe@users.noreply.github.com>
2022-08-02 20:43:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,10 +10,8 @@ app = Flask(__name__)
 SECRET_KEY = 'SPARTA'
 
 from pymongo import MongoClient
-
-client = MongoClient('mongodb+srv://test:sparta@cluster0.fihmw.mongodb.net/?retryWrites=true&w=majority')
+client = MongoClient('mongodb+srv://test:sparta@cluster0.cadiwnl.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
-
 
 
 @app.route('/')
@@ -21,13 +19,12 @@ def home():
     token_receive = request.cookies.get('mytoken')
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        user_info = db.user.find_one({"username": payload["id"]})
+        user_info = db.user.find_one({"id": payload["id"]})
         return render_template('index.html', user_info=user_info)
     except jwt.ExpiredSignatureError:
         return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
     except jwt.exceptions.DecodeError:
         return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
-
 
 
 @app.route('/login')
@@ -45,26 +42,21 @@ def register():
 
 @app.route('/api/register', methods=['POST'])
 def api_register():
-    username_receive = request.form['username_give']
+    id_receive = request.form['id_give']
     pw_receive = request.form['pw_give']
     name_receive = request.form['name_give']
 
     pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
 
-    db.user.insert_one({'username': username_receive, 'pw': pw_hash, 'name': name_receive})
+    db.user.insert_one({'id': id_receive, 'pw': pw_hash, 'name': name_receive})
 
     return jsonify({'result': 'success'})
 
+#id가 DB에 저장된것 중에 있으면 중복으로 불가능함을 나타냄
 @app.route('/sign_up/check_dup', methods=['POST'])
 def check_dup():
-    username_receive = request.form['username_give']
-    exists = bool(db.user.find_one({"username": username_receive}))
-    return jsonify({'result': 'success', 'exists': exists})
-
-@app.route('/sign_up/check_nickdup', methods=['POST'])
-def check_nickdup():
-    nickname_receive = request.form['nickname_give']
-    exists = bool(db.user.find_one({"nick": nickname_receive}))
+    id_receive = request.form['id_give']
+    exists = bool(db.user.find_one({"id": id_receive}))
     return jsonify({'result': 'success', 'exists': exists})
 
 
@@ -117,4 +109,4 @@ def api_valid():
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5001, debug=True)
